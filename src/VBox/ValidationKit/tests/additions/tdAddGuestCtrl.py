49febe6287824392b3c65c74d357323fd52ad30c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-05-14 16:58:27 +0000

    tests/additions/tdAddGuestCtrl.py: Fixed the path of VBoxGuestInstallHelper.exe for newer builds. bugref:10881

--- src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
+++ src/VBox/ValidationKit/tests/additions/tdAddGuestCtrl.py
@@ -1747,8 +1747,12 @@ class SubTstDrvAddGuestCtrl(base.SubTestDriverBase):
             # For newer revisions we use VBoxGuestInstallHelper.exe. Should work on all Windows versions.
             if  self.oTstDrv.fpApiVer >= 7.0 \
             and self.oTstDrv.isVersionEqualOrBigger(self.oTstDrv.getGuestAdditionsRevision(oSession), "166162"):
-                # Right now we ASSUME that the installation directory always is the following:
-                sRegEditorExePath     = 'C:\\Program Files\\Oracle\\VirtualBox Guest Additions\\VBoxGuestInstallHelper.exe';
+                sRegEditorExeBasePath = 'C:\\Program Files\\Oracle\\VirtualBox Guest Additions\\';
+                # Right now we ASSUME that the installation directory always is one of the the following:
+                if self.oTstDrv.isVersionEqualOrBigger(self.oTstDrv.getGuestAdditionsRevision(oSession), "168202"):
+                    sRegEditorExePath = sRegEditorExeBasePath + 'Tools\\VBoxGuestInstallHelper.exe'; # pylint: disable=line-too-long
+                else:
+                    sRegEditorExePath = sRegEditorExeBasePath + 'VBoxGuestInstallHelper.exe';
                 asRegEditorArgs       = [ sRegEditorExePath, 'registry', 'write', 'HKLM',
                                          'SYSTEM\\CurrentControlSet\\Services\\VBoxService', 'ImagePath', 'REG_SZ',
                                          sImagePath ];
