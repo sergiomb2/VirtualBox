ccfc74e5db282c3b2fbbee96ae2124fc6c1bd840
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-05-14 14:46:37 +0000

    libs/lwip-2.2.1: Build integration, bugref:10899 [build fix]

--- src/libs/lwip-2.2.1/vbox/include/arch/cc.h
+++ src/libs/lwip-2.2.1/vbox/include/arch/cc.h
@@ -14,13 +14,6 @@
 # define LWIP_TIMEVAL_PRIVATE 0
 #endif
 
-typedef uint8_t u8_t;
-typedef int8_t s8_t;
-typedef uint16_t u16_t;
-typedef int16_t s16_t;
-typedef uint32_t u32_t;
-typedef int32_t s32_t;
-
 typedef uintptr_t mem_ptr_t;
 
 #ifdef _MSC_VER
