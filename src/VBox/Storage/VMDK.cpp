0d4d0a2c926362941b4297bcedd7adea5854175e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-05-28 14:43:19 +0000

    Storage/VMDK: fix docs error. bugref:8707

--- src/VBox/Storage/VMDK.cpp
+++ src/VBox/Storage/VMDK.cpp
@@ -8410,9 +8410,11 @@ static int vmdkReplaceExtentSize(PVMDKIMAGE pImage, PVMDKEXTENT pExtent, unsigne
  * Used for sparse extent resize.
  *
  * @returns VBox status code.
- * @param   pImage          VMDK image instance.
- * @param   pExtent         VMDK extent instance.
- * @param   cSectorsNew     Number of sectors after resize.
+ * @param   pImage                  VMDK image instance.
+ * @param   pExtent                 VMDK extent instance.
+ * @param   cSectorsNew             Number of sectors after resize.
+ * @param   cNewOverheadSectors     The size of new overhead in sectors. This
+ *                                  includes the header and all grain tables/directories.
  */
 static int vmdkRelocateSectorsForSparseResize(PVMDKIMAGE pImage, PVMDKEXTENT pExtent,
                                               uint64_t cSectorsNew, uint64_t cNewOverheadSectors)
